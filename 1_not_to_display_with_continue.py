4b04856c2465d5a00752b9b0c3bc8e62e3adf49c
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-11-03 23:29:09 +0530

    Add files via upload

---
+++ 1_not_to_display_with_continue.py
@@ -0,0 +1,50 @@
+# n=123
+# while n>=12:
+#     if n==134:
+#         continue
+#     print(n)
+#     n+=1
+
+
+# n=0
+# while n<10:
+#     n+=1
+#     if n==6:
+#         continue
+#     print(n)
+
+
+""" 2nd type"""
+"""
+n=0
+while n<10:
+    n+=1
+    if n<5:
+        continue
+    print(n)
+"""
+
+""" 3rd type """
+"""
+n=0
+while n<10:
+    n+=1
+    if n>5:
+        continue
+    print(n)
+"""
+
+""" 4th type  """
+"""
+n=0
+while n<10:
+    n+=1
+    if n<=5:
+        continue
+    print(n)
+"""
+
+# if (11>2):
+#     pass
+# else:
+#     print('False')
